9bd4e268e7acdcd1a09fbd0e857a361b9847a070
Lavrik <Lavrenenok89@mail.ru>
2024-01-03 22:19:35 +0800

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -7,28 +7,31 @@ function Student(name, gender, age) {
 
 Student.prototype.setSubject = function (subjectName) {
    this.subject = subjectName;
-}
+};
 
 Student.prototype.addMarks = function (...marksToAdd) {
-   if (!this.hasOwnProperty('marks') || this.excluded) {
-      console.log('Студент отчислен или не установлен предмет.');
+   if (this.marks === undefined) {
+      console.log(`${this.name} is excluded and cannot receive marks.`);
       return;
    }
-
    this.marks.push(...marksToAdd);
-}
+   console.log(`Marks added for ${this.name}: ${marksToAdd}`);
+};
+
 Student.prototype.getAverage = function () {
-   if (!this.hasOwnProperty('marks') || this.marks.length === 0 || this.excluded) {
-      console.log('Нет оценок или студент отчислен.');
+   if (!this.marks || this.marks.length === 0) {
+      console.log(`${this.name} has no marks.`);
       return 0;
    }
-
    const sum = this.marks.reduce((total, mark) => total + mark, 0);
-   return sum / this.marks.length;
-}
+   const average = sum / this.marks.length;
+   console.log(`Average marks for ${this.name}: ${average.toFixed(2)}`);
+   return average;
+};
 
 Student.prototype.exclude = function (reason) {
    delete this.subject;
    delete this.marks;
    this.excluded = reason;
-}
+   console.log(`${this.name} is excluded from the educational process. Reason: ${reason}`);
+};
